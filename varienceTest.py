0566bf63bd06eb15c004a0f105eaad7afbf39d16
Matt Hennigan <matthenni@hotmail.co.uk>
2015-12-16 21:16:18 +0000

    Add ANSI escape code functionality
    This keeps the program output clear and easy to read.

--- varienceTest.py
+++ varienceTest.py
@@ -36,6 +36,34 @@ count_for_kings = 0
 aceCount = []
 kingCount = []
 
+def clear_terminal():
+    """
+    'clear_terminal()' uses ANSI escape codes to clear the screen.
+    https://en.wikipedia.org/wiki/ANSI_escape_code
+    """
+
+    CSI = chr(27)
+
+    # 1) We need to clear the entire screen. Note this leaves the cursor
+    # wherever it was.
+
+    # CSI "[" n "J"
+    # Clears part of the screen. If n is 0 (or missing), clear from cursor to
+    # end of screen. If n is 1, clear from cursor to beginning of the screen.
+    # If n is 2, clear entire screen (and moves cursor to upper left on
+    # DOS ANSI.SYS).
+    print(CSI + "[2J", end='')
+
+    # 2) Move the cursor back to the top left corner so the next time we print
+    # something it appears there.
+
+    # CSI "[" n ";" m "H"
+    # Moves the cursor to row n, column m. The values are 1-based, and default
+    # to 1 (top left corner) if omitted. A sequence such as CSI ;5H is a synonym
+    # for CSI 1;5H as well as CSI 17;H is the same as CSI 17H and CSI 17;1H
+    print(CSI + "[H", end='')
+
+
 while True:
     # Deal the player's first card.
     firstCard = random.choice(possibles)
@@ -67,10 +95,21 @@ while True:
                 ace_num = len(aceCount)
                 # Calculate how often the player receives pocket aces.
                 average_acecount = sum(aceCount) / ace_num
+                # Number of times pockets aces have been received.
+                king_num = len(kingCount)
+                # Exception handling for the case that king_num is equal to zero.
+                if king_num == 0:
+                    average_kingcount = sum(kingCount) / 1
+                else:
+                    average_kingcount = sum(kingCount) / king_num
                 # Display hand information
                 print("Average Ace Count:", average_acecount)
                 print("Pocket aces received", ace_num, "times.")
                 print("Ace Variance:", (average_acecount / 220), "\n")
+                print("Average King Count:", average_kingcount)
+                print("Pocket Kings received", king_num, "times.")
+                print("King Variance:", (average_kingcount / 220), "\n")
+                clear_terminal()
             
             if "King" in firstCard and "King" in secondCard:
                 # print("You received pocket kings on hand number:", count_for_kings)
@@ -81,7 +120,17 @@ while True:
                 king_num = len(kingCount)
                 # Calculate how often the player receives pocket kings.
                 average_kingcount = sum(kingCount) / king_num
-                # Display hand information.
+                # Number of times pockets kings have been received.
+                ace_num = len(aceCount)
+                # Exception handling for the case that ace_num is equal to zero.
+                if ace_num == 0:
+                    average_acecount = sum(aceCount) / 1
+                else:
+                    average_acecount = sum(aceCount) / ace_num
+                print("Average Ace Count:", average_acecount)
+                print("Pocket aces received", ace_num, "times.")
+                print("Ace Variance:", (average_acecount / 220), "\n")
                 print("Average King Count:", average_kingcount)
                 print("Pocket Kings received", king_num, "times.")
-                print("King Variance:", (average_kingcount / 220), "\n")               
+                print("King Variance:", (average_kingcount / 220), "\n")    
+                clear_terminal()       
